159c8562d77bc96d396117a44606e22ebff4a46c
Kyaw Myo Aung <kyawmyoaung.dev@gmail.com>
2022-07-07 05:32:35 +0630

    feat: add gallery page

--- gallery.php
+++ gallery.php
@@ -1,8 +1,140 @@
 <?php  include '_header.php' ?>
 
 
-<div class="container">
-    <h1>gallery page</h1>
-</div>
+  <!-- gallery page start -->
+  <div class="gallery">
+        <div class="container">
+            <h3 class="text-center title">Our Products</h3>
+
+            <div class="row my-3">
+                <div class="col-12">
+                    <div class="product_categories">
+                        <a class="active" href="#">All</a> 
+                        <a href="#">BARBELLS</a>  
+                        <a href="#">RACKS</a>  
+                        <a href="#">BENCHES</a>
+                        <a href="#">DUMBBELLS</a>
+                    </div>
+                </div>
+            </div>
+            <div class="row mb-3">
+                <div class="col-lg-3 col-md-3 col-sm-12">
+                    <div class="product_container">
+                        <div class="product_img">
+                            <span class="product_type">new</span>
+                            <img class="img-thumbnail" src="./images/product_1.jpg" alt="">
+                        </div>
+                        <h3 class="product_description">WEIGHT BENCHES</h3>
+                        <span class="product_price">Price : $100</span>
+                        <span class="product_code">item code : 001</span>
+                        <span class="product_category">category : barbell</span>
+
+                        <input type="submit" class="btn btn_add_to_card" value="add to card">
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-12">
+                    <div class="product_container">
+                        <div class="product_img">
+                            <span class="product_type">new</span>
+                            <img class="img-thumbnail" src="./images/product_1.jpg" alt="">
+                        </div>
+                        <h3 class="product_description">WEIGHT BENCHES</h3>
+                        <span class="product_price">Price : $100</span>
+                        <span class="product_code">item code : 001</span>
+                        <span class="product_category">category : barbell</span>
+
+                        <input type="submit" class="btn btn_add_to_card" value="add to card">
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-12">
+                    <div class="product_container">
+                        <div class="product_img">
+                            <span class="product_type">new</span>
+                            <img class="img-thumbnail" src="./images/product_1.jpg" alt="">
+                        </div>
+                        <h3 class="product_description">WEIGHT BENCHES</h3>
+                        <span class="product_price">Price : $100</span>
+                        <span class="product_code">item code : 001</span>
+                        <span class="product_category">category : barbell</span>
+
+                        <input type="submit" class="btn btn_add_to_card" value="add to card">
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-12">
+                    <div class="product_container">
+                        <div class="product_img">
+                            <span class="product_type">new</span>
+                            <img class="img-thumbnail" src="./images/product_1.jpg" alt="">
+                        </div>
+                        <h3 class="product_description">WEIGHT BENCHES</h3>
+                        <span class="product_price">Price : $100</span>
+                        <span class="product_code">item code : 001</span>
+                        <span class="product_category">category : barbell</span>
+
+                        <input type="submit" class="btn btn_add_to_card" value="add to card">
+                    </div>
+                </div>
+            </div>
+            <div class="row mb-3">
+                <div class="col-lg-3 col-md-3 col-sm-12">
+                    <div class="product_container">
+                        <div class="product_img">
+                            <span class="product_type">second hand</span>
+                            <img class="img-thumbnail" src="./images/product_1.jpg" alt="">
+                        </div>
+                        <h3 class="product_description">WEIGHT BENCHES</h3>
+                        <span class="product_price">Price : $100</span>
+                        <span class="product_code">item code : 001</span>
+                        <span class="product_category">category : barbell</span>
+
+                        <input type="submit" class="btn btn_add_to_card" value="add to card">
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-12">
+                    <div class="product_container">
+                        <div class="product_img">
+                            <span class="product_type">second hand</span>
+                            <img class="img-thumbnail" src="./images/product_1.jpg" alt="">
+                        </div>
+                        <h3 class="product_description">WEIGHT BENCHES</h3>
+                        <span class="product_price">Price : $100</span>
+                        <span class="product_code">item code : 001</span>
+                        <span class="product_category">category : barbell</span>
+
+                        <input type="submit" class="btn btn_add_to_card" value="add to card">
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-12">
+                    <div class="product_container">
+                        <div class="product_img">
+                            <span class="product_type">second hand</span>
+                            <img class="img-thumbnail" src="./images/product_1.jpg" alt="">
+                        </div>
+                        <h3 class="product_description">WEIGHT BENCHES</h3>
+                        <span class="product_price">Price : $100</span>
+                        <span class="product_code">item code : 001</span>
+                        <span class="product_category">category : barbell</span>
+
+                        <input type="submit" class="btn btn_add_to_card" value="add to card">
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-12">
+                    <div class="product_container">
+                        <div class="product_img">
+                            <span class="product_type">second hand</span>
+                            <img class="img-thumbnail" src="./images/product_1.jpg" alt="">
+                        </div>
+                        <h3 class="product_description">WEIGHT BENCHES</h3>
+                        <span class="product_price">Price : $100</span>
+                        <span class="product_code">item code : 001</span>
+                        <span class="product_category">category : barbell</span>
+
+                        <input type="submit" class="btn btn_add_to_card" value="add to card">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- gallery page end -->
 
 <?php  include 'footer.php' ?>
